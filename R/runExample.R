b1f31b8c9f093acc888b522f6d5e31a2718c5db7
jonaitis <jonaitis@wisc.edu>
2023-08-29 13:54:31 -0500

    Updated documentation and rebuilt package

--- R/runExample.R
+++ R/runExample.R
@@ -2,7 +2,7 @@
 runExample <- function() {
   appDir <- system.file("shiny-examples", "WisNormPlot", package = "WisNorms")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `WisNorms`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `WisNorms` package.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
